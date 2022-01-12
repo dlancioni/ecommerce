9208b0971df79ab3709770b0dc2669b7f5d9d3af
David Lancioni <dlancioni@gmail.com>
2022-01-12 17:54:54 -0300

    Add modal

--- store/controller/product.py
+++ store/controller/product.py
@@ -13,14 +13,14 @@ class Product(Base):
         super().__init__(db, session, request, form)
         
     def get_total_product_by_category(self):
-        rs = db.session.query(c.name, db.func.count(p.id).label("total")).join(p).group_by(c.name)
+        rs = db.session.query(c.id, c.name, db.func.count(p.id).label("total")).join(p).group_by(c.name)
         return rs        
 
     def get_product_by_category(self, category_id = 0):
-        rs = p.query.filter(p.category_id == category_id).all()
+        rs = p.query.filter(p.category_id == category_id).order_by(p.name).all()
         return rs
 
     def get_product(self, description = ""):
         v = "%" + description + "%"
-        rs = p.query.filter(or_(p.name.like(v), p.description.like(v))).all()
+        rs = p.query.filter(or_(p.name.like(v), p.description.like(v))).order_by(p.name).all()
         return rs
