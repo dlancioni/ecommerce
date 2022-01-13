75a330ccf276b2ee471e1abc4b5a12356a08ea28
David Lancioni <dlancioni@gmail.com>
2022-01-13 14:34:23 -0300

    Add product and shopping cart

--- store/routes.py
+++ store/routes.py
@@ -7,11 +7,18 @@ from store.controller.store import Store
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
     form1 = FormStore()
     store = Store(db, session, request, form1)
-    rs1, rs2 = store.get_data()
-    
+    rs1, rs2 = store.get_data()    
     return render_template("store.html", form_home=form1, 
                                          category=rs1, 
                                          products=rs2)
+    
+@app.route("/product", methods=["GET", "POST"])
+def product():
+    form1 = FormStore()
+    store = Store(db, session, request, form1)
+    rs1, rs2 = store.get_data()    
+    return render_template("product.html", form_home=form1,
+                                           category=rs1, 
+                                           products=rs2)
