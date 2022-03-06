ab293359a2c4c26e3d1aba7725192c899aa1c2f9
David Lancioni <dlancioni@gmail.com>
2022-03-06 11:22:59 -0300

    Create temp area

--- store/controller/temp.py
+++ store/controller/temp.py
@@ -1,12 +1,10 @@
 from flask import render_template
 from store.controller.base import Base
-from store.models.category import Category as category
 
-class Test(Base):
+class Temp(Base):
 
     def __init__(self, db, session=None, request=None, form=None):
         super().__init__(db, session, request, form)
 
-    def do(self):
-        # return "Teste"
-        return render_template("test.html", param=123)
+    def temp(self, value):
+        return render_template("temp.html", param=value)
--- store/routes.py
+++ store/routes.py
@@ -3,6 +3,7 @@ from store import app
 from flask import session, request
 from store.forms.store import FormStore
 from store.controller.store import Store
+from store.controller.temp import Temp
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,3 +12,7 @@ def home():
 @app.route("/product/<int:id>", methods=["GET", "POST"])
 def product(id):
     return Store(db, session, request, FormStore()).product(id)
+
+@app.route("/temp", methods=["GET", "POST"])
+def temp():
+    return Temp(db, session, request, FormStore()).temp(1234)
