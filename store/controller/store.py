977281d92e182ad6dd675cc4a3a1c49696fd478a
David Lancioni <dlancioni@gmail.com>
2022-03-06 11:16:58 -0300

    Rename form

--- store/controller/store.py
+++ store/controller/store.py
@@ -1,10 +1,7 @@
 from store import db
 from flask import render_template, session, request
-from store.forms.store import FormStore
 from store.controller.base import Base
-from store.controller.category import Category
 from store.controller.product import Product
-
 
 class Store(Base):
 
@@ -16,13 +13,13 @@ class Store(Base):
         search = self.form.searchbar.data or ""
         rs1, rs2 = Product(db).get_data(category_id, search)
         return render_template("store.html", action = "list", 
-                                             form_home = self.form,
+                                             form = self.form,
                                              category = rs1,
                                              products = rs2)
         
     def product(self, id):
         rs1, rs2 = Product(db).get_product_details(id)
         return render_template("store.html", action = "product",
-                                             form_home = self.form,
+                                             form = self.form,
                                              category = rs1, 
                                              product = rs2)
