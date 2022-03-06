45cbc7ac8df9a5fea35a50f4926e644f9fcb691e
David Lancioni <dlancioni@gmail.com>
2022-03-06 15:35:47 -0300

    Fix form submission

---
+++ store/controller/cart.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from store.controller.base import Base
+
+class Cart(Base):
+
+    def __init__(self, db, session=None, request=None, form=None):
+        super().__init__(db, session, request, form)
+
+    def add_cart(self):
+        return "Adiconado no carrinho"
--- store/routes.py
+++ store/routes.py
@@ -3,6 +3,7 @@ from store import app
 from flask import session, request
 from store.forms.home import FormHome
 from store.controller.home import Home
+from store.controller.cart import Cart
 from store.controller.temp import Temp
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,6 +13,10 @@ def home():
 @app.route("/product/<int:id>", methods=["GET", "POST"])
 def product(id):
     return Home(db, session, request, FormHome()).product(id)
+
+@app.route("/addcart", methods=["GET", "POST"])
+def addcart():
+    return Cart(db, session, request, FormHome()).add_cart()
 
 @app.route("/temp", methods=["GET", "POST"])
 def temp():
