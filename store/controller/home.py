688d5edc40e59227ccfe92771cfc921f758af420
David Lancioni <dlancioni@gmail.com>
2022-03-06 14:04:28 -0300

    Fix class

--- store/controller/home.py
+++ store/controller/home.py
@@ -9,17 +9,11 @@ class Home(Base):
         super().__init__(db, session, request, form)
        
     def home(self):
-        category_id = self.request.args.get("category_id", type = int) or 0
+        category_id = self.request.args.get("category_id", type=int) or 0
         search = self.form.searchbar.data or ""
         rs1, rs2 = Product(db).get_data(category_id, search)
-        return render_template("home.html", action = "list", 
-                                            form = self.form,
-                                            category = rs1,
-                                            products = rs2)
+        return render_template("home.html", form=self.form, category=rs1, products=rs2)
         
     def product(self, id):
         rs1, rs2 = Product(db).get_product_details(id)
-        return render_template("product.html", action = "product",
-                                               form = self.form,
-                                               category = rs1,
-                                               product = rs2)
+        return render_template("product.html", form=self.form, category=rs1, product=rs2)
