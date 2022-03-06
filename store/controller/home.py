72dc772782e5c41d7a5dec26d926dfa99e9d04c3
David Lancioni <dlancioni@gmail.com>
2022-03-06 11:40:58 -0300

    FormStore to FormHome

--- store/controller/home.py
+++ store/controller/home.py
@@ -1,5 +1,5 @@
 from store import db
-from flask import render_template, session, request
+from flask import render_template
 from store.controller.base import Base
 from store.controller.product import Product
 
@@ -12,14 +12,14 @@ class Home(Base):
         category_id = self.request.args.get("category_id", type = int) or 0
         search = self.form.searchbar.data or ""
         rs1, rs2 = Product(db).get_data(category_id, search)
-        return render_template("store.html", action = "list", 
-                                             form = self.form,
-                                             category = rs1,
-                                             products = rs2)
+        return render_template("home.html", action = "list", 
+                                            form = self.form,
+                                            category = rs1,
+                                            products = rs2)
         
     def product(self, id):
         rs1, rs2 = Product(db).get_product_details(id)
-        return render_template("store.html", action = "product",
-                                             form = self.form,
-                                             category = rs1, 
-                                             product = rs2)
+        return render_template("home.html", action = "product",
+                                            form = self.form,
+                                            category = rs1,
+                                            product = rs2)
--- store/routes.py
+++ store/routes.py
@@ -1,18 +1,18 @@
 from store import db
 from store import app
 from flask import session, request
-from store.forms.store import FormStore
+from store.forms.home import FormHome
 from store.controller.home import Home
 from store.controller.temp import Temp
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return Home(db, session, request, FormStore()).home()
+    return Home(db, session, request, FormHome()).home()
     
 @app.route("/product/<int:id>", methods=["GET", "POST"])
 def product(id):
-    return Home(db, session, request, FormStore()).product(id)
+    return Home(db, session, request, FormHome()).product(id)
 
 @app.route("/temp", methods=["GET", "POST"])
 def temp():
-    return Temp(db, session, request, FormStore()).temp(1234)
+    return Temp(db, session, request, FormHome()).temp(1234)
