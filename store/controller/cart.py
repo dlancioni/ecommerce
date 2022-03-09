a5032732bb86776ac67c4499422d5d3f3748f948
David Lancioni <dlancioni@gmail.com>
2022-03-08 22:26:22 -0300

    Shopping cart

--- store/controller/cart.py
+++ store/controller/cart.py
@@ -1,4 +1,5 @@
-from flask import render_template
+import flask
+from flask import render_template, redirect, url_for
 from store.controller.base import Base
 
 class Cart(Base):
@@ -16,31 +17,40 @@ class Cart(Base):
         product = [id, name, qtt, price, total]
         return product
 
-    def add(self, product):
-        if self.find(product[0]) == -1:
-            self.products.append(product)
+    def add(self, new):
+        old, index = self.find(new[0])
+        if old == []:
+            self.products.append(new)
+        else:
+            old[2] = old[2] + new[2]
+            old[3] = new[3]
+            old[4] = old[2] * old[3]
 
     def remove(self, id):
-        index = self.find(id)
-        if index >= 0:
+        product, index = self.find(id)
+        if product != []:
             self.products.pop(index)
 
     def find(self, id):
+        index = 0
+        product = []
         for index, value in enumerate(self.products):
             if int(value[0]) == int(id): 
-                return index
-        return -1
+                product = value
+                break
+        return product, index
 
     def cart(self):
         self.products = self.session['CART_IN']
         return render_template("cart.html", form=self.form, cart_in=self.products)
 
-    def add_cart(self):        
-        self.products = self.session['CART_IN']
-        product = self.create(self.request.form)
-        self.add(product)
-        self.session['CART_IN'] = self.products
-        return render_template("cart.html", form=self.form, cart_in=self.products)
+    def add_cart(self):
+        if flask.request.method == "POST":
+            self.products = self.session['CART_IN']
+            product = self.create(self.request.form)
+            self.add(product)
+            self.session['CART_IN'] = self.products
+            return redirect(url_for("list_cart"))
 
     def remove_cart(self, id):
         self.products = self.session['CART_IN']
